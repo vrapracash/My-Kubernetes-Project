af15c78a8fec3fc3715c08f4b0f69c9a3c5a0daf
Veera Prakash <veeraprakash.bg@gmail.com>
2026-02-12 10:10:36 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,9 +1,13 @@
-terraform {
-  backend "s3" {
-    bucket         = "org-terraform-state-bucket"
-    key            = "eks/uat/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
+provider "aws" {
+  region = var.region
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_ca)
+  token                  = data.aws_eks_cluster_auth.this.token
 }
