c21a343ef218beddbd0fa947569038805bed58b4
Veera Prakash <veeraprakash.bg@gmail.com>
2026-02-12 10:55:16 +0530

    Update main.tf

--- modules/nodegroups/main.tf
+++ modules/nodegroups/main.tf
@@ -1,19 +1,57 @@
-module "managed_nodegroup" {
+locals {
+  instance_type = "m5.large"
+}
+
+# Platform nodes
+module "platform_nodes" {
   source  = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
 
+  name         = "uat-platform"
   cluster_name = var.cluster_name
   subnet_ids   = var.subnets
 
-  name = "uat-app-nodes"
-
-  instance_types = ["m5.large"]
-
-  min_size     = 2
-  max_size     = 8
-  desired_size = 3
+  instance_types = [local.instance_type]
+  min_size       = 2
+  max_size       = 4
+  desired_size   = 2
 
   labels = {
-    environment = "uat"
-    workload    = "applications"
+    workload = "platform"
+  }
+}
+
+# Backend nodes
+module "backend_nodes" {
+  source  = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
+
+  name         = "uat-backend"
+  cluster_name = var.cluster_name
+  subnet_ids   = var.subnets
+
+  instance_types = [local.instance_type]
+  min_size       = 3
+  max_size       = 10
+  desired_size   = 5
+
+  labels = {
+    workload = "backend"
+  }
+}
+
+# Frontend nodes
+module "frontend_nodes" {
+  source  = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
+
+  name         = "uat-frontend"
+  cluster_name = var.cluster_name
+  subnet_ids   = var.subnets
+
+  instance_types = [local.instance_type]
+  min_size       = 2
+  max_size       = 6
+  desired_size   = 3
+
+  labels = {
+    workload = "frontend"
   }
 }
