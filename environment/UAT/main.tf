366da9a46bb656b8aa844a06178d8f4cacd88c7a
Veera Prakash <veeraprakash.bg@gmail.com>
2026-02-12 10:13:15 +0530

    Create environment/UAT/main.tf

---
+++ environment/UAT/main.tf
@@ -0,0 +1,21 @@
+module "eks" {
+  source          = "../../modules/eks"
+  cluster_name    = "eks-uat"
+  vpc_id           = var.vpc_id
+  private_subnets  = var.private_subnets
+}
+
+module "nodegroups" {
+  source       = "../../modules/nodegroups"
+  cluster_name = module.eks.cluster_name
+  subnets      = var.private_subnets
+}
+
+module "addons" {
+  source       = "../../modules/addons"
+  cluster_name = module.eks.cluster_name
+}
+
+module "namespaces" {
+  source = "../../modules/namespaces"
+}
