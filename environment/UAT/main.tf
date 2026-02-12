b64a7cdc26eaed7d93299dce4d7819622b90f608
Veera Prakash <veeraprakash.bg@gmail.com>
2026-02-12 10:47:17 +0530

    Update main.tf

--- environment/UAT/main.tf
+++ environment/UAT/main.tf
@@ -1,14 +1,14 @@
 module "eks" {
-  source          = "../../modules/eks"
-  cluster_name    = "eks-uat"
-  vpc_id           = var.vpc_id
-  private_subnets  = var.private_subnets
+  source           = "../../modules/eks"
+  cluster_name     = "eks-uat"
+  vpc_id            = var.vpc_id
+  private_subnets   = var.private_subnets
 }
 
 module "nodegroups" {
   source       = "../../modules/nodegroups"
   cluster_name = module.eks.cluster_name
-  subnets      = var.private_subnets
+  subnets       = var.private_subnets
 }
 
 module "addons" {
@@ -18,4 +18,8 @@ module "addons" {
 
 module "namespaces" {
   source = "../../modules/namespaces"
+}
+
+module "iam_irsa" {
+  source = "../../modules/iam"
 }
